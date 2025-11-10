fe546848c195857c3c6ef78396afa89cdb4f2190
Sokerimunkki2035 <voinkosaadaveden@gmail.com>
2025-11-10 16:50:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,27 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("v1").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./aiti-hanhi.html",
+        "./aasi-pappa.html",
+        "./kahvipoyta.html",
+        "./manifest.json"
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
